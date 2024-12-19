214269a448bc9c7e3fb30b5ef14a0dbb737fbf71
stealacamera <113174127+stealacamera@users.noreply.github.com>
2024-12-19 13:07:38 +0100

    Added owner relation to post entity

---
+++ Blog/Blog.Infrastructure/Migrations/20241218205520_AddedPostOwnerFK.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPostOwnerFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_OwnerId",
+                table: "Posts",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_OwnerId",
+                table: "Posts",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Users_OwnerId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_OwnerId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Posts");
+        }
+    }
+}
